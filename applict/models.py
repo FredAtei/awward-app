1ebfecb46b31ad5dec659438f4a9e2225aa9041d
Fred <akamaatei@gmail.com>
2020-11-29 20:36:08 +0300

    make database

--- applict/models.py
+++ applict/models.py
@@ -3,27 +3,29 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-class Picture(models.Model):
-  image = CloudinaryField('image')
-
+from django.core.exceptions import ObjectDoesNotExist
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = CloudinaryField('image')
-    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    bio = models.TextField(max_length=700, default="Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
     contact = models.EmailField(max_length=100, blank=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username} profile'
+
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
+           
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
--- applict/views.py
+++ applict/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from .models import Picture
+from .models import Profile
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
-    pictures = Picture.objects.all()
-    ctx = {'pictures': pictures}
-    return render(request, 'awards/index.html', ctx)
+    return render(request, 'awards/index.html')
