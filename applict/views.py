072068972a9b9d806b939b8f134083d2fad6a1bb
Fred <akamaatei@gmail.com>
2020-11-30 16:49:57 +0300

    ip corrections1

--- applict/views.py
+++ applict/views.py
@@ -24,17 +24,17 @@ def profile(request,profile_id):
     current_user = request.user
     if request.method == "POST":
         user_form = UpdateUserForm(request.POST )
-        prof_form = UpdateUserProfileForm(request.POST, request.FILES)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES)
         if user_form.is_valid() and prof_form.is_valid():
             user_form.save()
-            prof_form.save()
+            profile_form.save()
             bio = user.bio
     else:
         user_form = UpdateUserForm()
         prof_form = UpdateUserProfileForm()
     params = {
         'user_form': user_form,
-        'prof_form': prof_form, 
+        'profile_form': prof_form, 
         'profile':profile  
 
     }
@@ -46,16 +46,16 @@ def addprofile(request):
         prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and prof_form.is_valid():
             user_form.save()
-            prof_form.save()
+            profile_form.save()
             # return redirect('profile')
             return HttpResponseRedirect(request.path_info)
     else:
         user_form = UpdateUserForm(instance=request.user)
-        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+        profile_form = UpdateUserProfileForm(instance=request.user.profile)
 
     params = {   
         'user_form': user_form,
-        'prof_form': prof_form,
+        'profile_form': prof_form,
         
     }
     return render(request, 'add_profile.html', params )  
